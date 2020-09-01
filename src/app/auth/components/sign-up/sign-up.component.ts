a46598fd1378aafee1106c86e629c50622da1482
mihail <michael.glushkov.1996@gmail.com>
2020-09-01 15:54:18 +0300

    codestyle fix

--- src/app/auth/components/sign-up/sign-up.component.ts
+++ src/app/auth/components/sign-up/sign-up.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
 import { first } from 'rxjs/operators';
 
 import { UtilsService, UserService, NotificationsService } from 'app/shared/services';
-import { HttpErrorResponse } from '@angular/common/http';
 import { NotificationMessage } from 'app/shared/models';
 
 @Component({
