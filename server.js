f2cb7ef43572a215c33ab92c43ff2a109665b067
mihail <michael.glushkov.1996@gmail.com>
2020-09-01 11:21:17 +0300

    deploy settings

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-// server.js
 const express = require('express');
+const path = require('path');
 const app = express();
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/dist'));
-// Start the app by listening on the default
-// Heroku port
+
+app.use(express.static(__dirname + '/dist/itop1000-front'));
+
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/itop1000-front/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
