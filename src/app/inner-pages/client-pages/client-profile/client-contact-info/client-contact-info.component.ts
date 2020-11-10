4e140290708cb1aa582dfbb60b0daa0df5e8184a
SalimovaNellia <salimova.nellia@gmail.com>
2020-11-10 12:22:25 +0200

    Separated the location section

--- src/app/inner-pages/client-pages/client-profile/client-contact-info/client-contact-info.component.ts
+++ src/app/inner-pages/client-pages/client-profile/client-contact-info/client-contact-info.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
 
 import { UserService } from 'app/shared/services';
 import { UserInfo } from 'app/shared/models';
 import * as fromCore from 'app/core/reducers';
+import { ClientProfileService } from 'app/inner-pages/client-pages/client-profile/client-profile.service';
 
 @Component({
   selector: 'app-client-contact-info',
@@ -13,11 +14,15 @@ import * as fromCore from 'app/core/reducers';
 })
 export class ClientContactInfoComponent implements OnInit {
 
-  public isEdit: boolean;
+  public isAccountEdit: boolean;
+  public isLocationEdit: boolean;
   public userInfo$: Observable<UserInfo>;
+
+  @Output() updateProfileInfo = new EventEmitter();
 
   constructor(
     private userService: UserService,
+    private clientProfileService: ClientProfileService,
     private store: Store<fromCore.State>,
   ) { }
 
@@ -25,11 +30,23 @@ export class ClientContactInfoComponent implements OnInit {
     this.userInfo$ = this.store.select(fromCore.getUserInfo);
   }
 
-  public editToggle(): void {
-    this.isEdit = !this.isEdit;
+  public editAccountToggle(): void {
+    this.isAccountEdit = !this.isAccountEdit;
   }
 
-  public onSaveClick(userInfo: Partial<UserInfo>): void {
-    this.isEdit = false;
+  public editLocationToggle(): void {
+    this.isLocationEdit = !this.isLocationEdit;
   }
+
+  public onSaveAccountClick(userInfo: Partial<UserInfo>): void {
+    this.clientProfileService.onSaveClick(userInfo);
+    this.isAccountEdit = false;
+  }
+
+  public onSaveLocationClick(userInfo: Partial<UserInfo>): void {
+    this.clientProfileService.onSaveClick(userInfo);
+    this.isLocationEdit = false;
+  }
+
+  ngOnDestroy(): void {}
 }
