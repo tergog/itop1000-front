e322dc5974b9a392eb9498b7d67a8db666af0af5
SalimovaNellia <salimova.nellia@gmail.com>
2020-11-13 17:48:52 +0200

    code cleanup

--- src/app/inner-pages/client-pages/client-profile/client-posted-jobs/create-job/create-job.component.ts
+++ src/app/inner-pages/client-pages/client-profile/client-posted-jobs/create-job/create-job.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 import { JobsService } from 'app/shared/services';
 
 @Component({
