e79fc85e21cd30d1031601ed07a55af883ddca06
Almost Infinity <its.alfinity@gmail.com>
2020-12-15 18:39:34 +0200

    WYSIWYG layout

--- src/app/inner-pages/shared/components/chat/chat.module.ts
+++ src/app/inner-pages/shared/components/chat/chat.module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { SharedModule } from 'app/shared/shared.module';
 import { PagesSharedModule } from 'app/inner-pages/shared/pages-shared.module';
 import { ChatComponent } from './chat.component';
+import { WysiwygComponent } from './wysiwyg/wysiwyg.component';
 
 const routes: Routes = [
   { path: '', component: ChatComponent },
 ];
 
 @NgModule({
-  declarations: [ChatComponent],
+  declarations: [
+    ChatComponent,
+    WysiwygComponent
+  ],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
     SharedModule,
-    PagesSharedModule
+    PagesSharedModule,
+    FormsModule
   ]
 })
 export class ChatModule {
