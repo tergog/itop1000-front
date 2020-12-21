176dafd146e93433bd0bfc0006520f2e970d574e
Almost Infinity <its.alfinity@gmail.com>
2020-12-21 16:51:25 +0200

    Stable wysiwyg

--- src/app/inner-pages/shared/components/chat/rich-text-editor/rich-text-editor.module.ts
+++ src/app/inner-pages/shared/components/chat/rich-text-editor/rich-text-editor.module.ts
@@ -1,15 +1,27 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+
 import { QuillConfig, QuillModule } from 'ngx-quill';
 
 import { RichTextEditorComponent } from './rich-text-editor.component';
+
+const quillConfig: QuillConfig = {
+  placeholder: 'Message',
+  theme: 'snow',
+  modules: {
+    toolbar: {
+      container: '.chat__text-editor__toolbar'
+    },
+    "emoji-toolbar": true
+  }
+};
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    QuillModule.forRoot()
+    QuillModule.forRoot(quillConfig)
   ],
   declarations: [ RichTextEditorComponent ],
   exports: [ RichTextEditorComponent ],
