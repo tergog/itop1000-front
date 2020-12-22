97fc2dd4ba912f66ef061fbf32357c1907b8dacd
Nikita Chernega <nikita.chernega1@gmail.com>
2020-12-22 16:46:28 +0200

    edits

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://itop1000-back.herokuapp.com/',
-  secret: 'itop100053eae3bb-0536-4bdd-96ac-441ae6b07f26E3143639-DF35-4B67-9DFC-D6A6CD3612D99007067D-D79C-491D-BE50-3391F58D7E4A'
+  apiUrl: 'https://itop1000-back.herokuapp.com/'
 };
