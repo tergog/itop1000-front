8973ca78c68ad3cebbc90dbfcb044ba2b55a1c0b
Almost Infinity <its.alfinity@gmail.com>
2020-12-30 17:21:54 +0200

    feat(chat): responsible design

---
+++ src/app/inner-pages/shared/components/chat/sidebar-rooms/sidebar-rooms.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-rooms',
+  templateUrl: './sidebar-rooms.component.html',
+  styleUrls: [ './sidebar-rooms.component.scss' ]
+})
+export class SidebarRoomsComponent {
+
+}
