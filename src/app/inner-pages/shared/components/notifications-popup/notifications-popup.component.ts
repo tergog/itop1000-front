b43c3a897017b0fca0b0af17e5ebe6e5eed7573a
Nikita Chernega <nikita.chernega1@gmail.com>
2021-01-11 10:45:44 +0200

    feat(notification-popup) add animations for notifications

---
+++ src/app/inner-pages/shared/components/notifications-popup/notifications-popup.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+
+import { opacityInOutAnimation } from 'app/shared/animations';
+import { animate, style, transition, trigger } from '@angular/animations';
+
+export interface Mock {
+  name: string;
+  message: string;
+  id: number;
+}
+
+@Component({
+  selector: 'app-notifications-popup',
+  templateUrl: './notifications-popup.component.html',
+  styleUrls: ['./notifications-popup.component.scss'],
+  animations: [ trigger('notification', [
+    transition(':enter', [
+      style({ opacity: 0, marginTop: '-51px' }),
+      animate('300ms', style({ opacity: 1, marginTop: '10px' })),
+    ]),
+    transition(':leave', [
+      animate('300ms', style({ opacity: 0, marginTop: '-51px' }))
+    ])
+  ])
+  ]
+})
+
+export class NotificationsPopupComponent implements OnInit {
+
+  mockData: Mock[] = [
+    {
+      name: 'Boris Stelman',
+      message: 'At vero eds et accusamus et iusto',
+      id: 1
+    },
+    {
+      name: 'Boris Stelman',
+      message: 'At vero eds et accusamus et iusto',
+      id: 2
+    },
+    {
+      name: 'Boris Stelman',
+      message: 'At vero eds et accusamus et iusto',
+      id: 3
+    },
+    {
+      name: 'Boris Stelman',
+      message: 'At vero eds et accusamus et iusto',
+      id: 4
+    },
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  delete(id: number): void {
+    this.mockData = this.mockData.filter(el => el.id !== id);
+    console.log(this.mockData);
+  }
+
+}
