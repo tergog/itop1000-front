1ba953aee1a67a054b6e5d7f9bf605c8b7b5032e
Nikita Chernega <nikita.chernega1@gmail.com>
2021-01-11 18:32:14 +0200

    refactor(app.module) move animation from notification to app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,24 @@ import { Component, OnInit } from '@angular/core';
 
 import { NotificationMessage } from 'app/shared/models';
 import { NotificationsService } from 'app/shared/services/notifications.service';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('notification', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('200ms ease-out', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        animate('200ms', style({ opacity: 0 }))
+      ])
+    ])
+  ]
 })
 export class AppComponent implements OnInit {
   title = 'ITOP1000';
--- src/app/core/components/notification/notification.component.ts
+++ src/app/core/components/notification/notification.component.ts
@@ -2,23 +2,11 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 import { NotificationMessage } from 'app/shared/models';
 import { ENotificationStatus } from 'app/shared/enums/notification-status.enum';
-import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-notification',
   templateUrl: './notification.component.html',
-  styleUrls: ['./notification.component.scss'],
-  animations: [
-    trigger('notification', [
-      transition(':enter', [
-        style({ opacity: 0, transform: 'translateX(400px)' }),
-        animate('300ms ease-out', style({ opacity: 1, transform: 'translateX(0)' })),
-      ]),
-      transition(':leave', [
-        animate('300ms', style({ opacity: 0, transform: 'translateX(400px)' }))
-      ])
-    ])
-  ]
+  styleUrls: ['./notification.component.scss']
 })
 export class NotificationComponent implements OnInit {
   /** message to show */
@@ -26,7 +14,7 @@ export class NotificationComponent implements OnInit {
   /** notification closed output event */
   @Output() notificationClosed = new EventEmitter();
   /** show notification message by default time */
-  public defaultTimeout = 5000;
+  public defaultTimeout = 500000;
   NotificationStatus = ENotificationStatus;
 
   constructor() {}
