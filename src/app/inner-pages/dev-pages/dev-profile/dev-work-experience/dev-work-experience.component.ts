e59f78e477f593f9fe9ec0bfd34602f847554129
vlavirus <androando777@gmail.com>
2021-01-15 14:10:54 +0200

    fix(create-job-component): add method to work with observer

--- src/app/inner-pages/dev-pages/dev-profile/dev-work-experience/dev-work-experience.component.ts
+++ src/app/inner-pages/dev-pages/dev-profile/dev-work-experience/dev-work-experience.component.ts
@@ -1,9 +1,9 @@
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { COMMA, ENTER } from '@angular/cdk/keycodes';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
-import { filter, first, tap } from 'rxjs/operators';
+import { Observable, Subject } from 'rxjs';
+import { filter, first, map, tap } from 'rxjs/operators';
 import xorBy from 'lodash.xorby';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { untilDestroyed } from 'ngx-take-until-destroy';
@@ -14,14 +14,15 @@ import { DevProperties, NameValueModel, UserInfo } from 'app/shared/models';
 import * as fromCore from 'app/core/reducers';
 import { UploadPhotoDialogComponent } from 'app/inner-pages/shared/components/upload-photo-dialog/upload-photo-dialog.component';
 import { UpdateUserProfileAction } from 'app/core/actions/core.actions';
-
+import { getDeveloperSkills } from 'app/core/developers/store/developers.actions';
+import * as fromDev from 'app/core/developers/store';
 
 @Component({
   selector: 'app-dev-work-experience',
   templateUrl: './dev-work-experience.component.html',
   styleUrls: ['./dev-work-experience.component.scss'],
 })
-export class DevWorkExperienceComponent implements OnInit, OnDestroy {
+export class DevWorkExperienceComponent implements OnInit, OnDestroy, AfterViewInit {
 
   public isNewProject: boolean;
   public form: FormGroup;
@@ -35,31 +36,38 @@ export class DevWorkExperienceComponent implements OnInit, OnDestroy {
   public projectImages: string[] = [];
 
   readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+  public allSkills: NameValueModel[] = [];
+  public selectedSkills = [];
+  // public availableTechnologies: NameValueModel[] = [
+  //
+  //   {name: 'Javascript', value: 1},
+  //   {name: 'Typescript', value: 2},
+  //   {name: 'CSS3', value: 3},
+  //   {name: 'HTML5', value: 5},
+  //   {name: 'AngularJS', value: 6},
+  //   {name: 'Angular 9', value: 7},
+  //   {name: 'Angular 10', value: 8},
+  //   {name: 'Angular 7', value: 9},
+  //   {name: 'Angular 8', value: 10},
+  //   {name: 'Angular 2+', value: 11},
+  //
+  // ];
 
-  public selectedTechnologies = [];
-  public availableTechnologies: NameValueModel[] = [
+  public availableSkills: Subject<NameValueModel[]> = new Subject<NameValueModel[]>();
 
-    {name: 'Javascript', value: 1},
-    {name: 'Typescript', value: 2},
-    {name: 'CSS3', value: 3},
-    {name: 'HTML5', value: 5},
-    {name: 'AngularJS', value: 6},
-    {name: 'Angular 9', value: 7},
-    {name: 'Angular 10', value: 8},
-    {name: 'Angular 7', value: 9},
-    {name: 'Angular 8', value: 10},
-    {name: 'Angular 2+', value: 11},
-
-  ];
+  public availableSkills$: Observable<NameValueModel[]> = this.availableSkills.asObservable().pipe(
+    map(val => this.allSkills.filter(skill => !val.find(item => item.value === skill.value))));
 
   constructor(
     private store: Store<fromCore.State>,
     private devProfileService: DevProfileService,
     private matDialog: MatDialog,
     private developersService: DevelopersService,
-    private utilsService: UtilsService
-  ) {
-  }
+    private utilsService: UtilsService,
+    private developerService: DevelopersService,
+    private storeDev: Store<fromDev.State>,
+    private cdr: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
     this.initForm();
@@ -69,9 +77,26 @@ export class DevWorkExperienceComponent implements OnInit, OnDestroy {
         console.log(userInfo);
         this.devProfileService.devProperties = userInfo.devProperties;
         this.userInfo = userInfo;
-        this.updateTechnologies(this.selectedTechnologies);
+        this.updateTechnologies(this.selectedSkills);
       })
     );
+
+    this.developerService.getDeveloperSkills().pipe(
+      first()
+    ).subscribe(value => this.storeDev.dispatch(getDeveloperSkills(value)));
+  }
+
+  ngAfterViewInit(): void {
+    this.storeDev.select(fromDev.getSkills)
+      .pipe(
+        filter(res => !!res.length),
+        first()
+      )
+      .subscribe(res => {
+        this.allSkills = res;
+        this.availableSkills.next([]);
+        this.cdr.detectChanges();
+      });
   }
 
   public onAddClick(): void {
@@ -92,24 +117,23 @@ export class DevWorkExperienceComponent implements OnInit, OnDestroy {
     this.showError = false;
     const newDevProperties: DevProperties = {projects: [...(this.userInfo.devProperties.projects || []), this.form.value]};
     this.userInfo = {...this.userInfo, devProperties: newDevProperties};
-    this.store.dispatch(new UpdateUserProfileAction(this.userInfo));   
+    this.store.dispatch(new UpdateUserProfileAction(this.userInfo));
     this.devProfileService.onSaveClick({devProperties: newDevProperties});
     this.isNewProject = false;
-    this.availableTechnologies.push(...this.selectedTechnologies);
-    this.selectedTechnologies = [];
+    this.selectedSkills = [];
   }
 
   public onTechnologySelect({ option }: any): void {
-    this.availableTechnologies = this.availableTechnologies.filter(technology => technology.value !== option.value.value);
-    this.selectedTechnologies.push(option.value);
-    this.form.get('technologies').patchValue(this.selectedTechnologies);
+    this.selectedSkills.push(option.value);
+    this.availableSkills.next(this.selectedSkills);
+    this.form.get('technologies').patchValue(this.selectedSkills);
     this.focusReset();
   }
 
   public onTechnologyRemove(technology: NameValueModel): void {
-    this.selectedTechnologies = this.selectedTechnologies.filter(item => item.value !== technology.value);
-    this.availableTechnologies.push(technology);
-    this.form.get('technologies').patchValue(this.selectedTechnologies);
+    this.selectedSkills = this.selectedSkills.filter(item => item.value !== technology.value);
+    this.availableSkills.next(this.selectedSkills);
+    this.form.get('technologies').patchValue(this.selectedSkills);
     this.focusReset();
   }
 
@@ -152,8 +176,9 @@ export class DevWorkExperienceComponent implements OnInit, OnDestroy {
   }
 
   private updateTechnologies(technologies: NameValueModel[]): void {
-    this.selectedTechnologies = [...technologies];
-    this.availableTechnologies = xorBy(this.selectedTechnologies, this.availableTechnologies);
+    this.selectedSkills = [...technologies];
+    // this.availableTechnologies = xorBy(this.selectedSkills, this.availableTechnologies);
+
   }
 
   private uploadImage(image: string, forLogo: boolean): void {
