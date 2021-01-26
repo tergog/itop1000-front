8b205a7c923938d78b686a857a278e9bd096f034
Almost Infinity <its.alfinity@gmail.com>
2021-01-26 15:24:56 +0200

    refactor(conversation.model): replace unreadCount by unread and total numbers

--- src/app/shared/models/conversation.model.ts
+++ src/app/shared/models/conversation.model.ts
@@ -4,6 +4,9 @@ export interface ConversationModel {
   id: string;
   createdAt: string;
   participants: ConversationMemberModel[];
-  unreadCount: number;
+  messages: {
+    unread: number,
+    total: number
+  };
   type: string;
 }
