5c38571075c388518ea73540e158d8e96837ea3e
Nikita Chernega <nikita.chernega1@gmail.com>
2021-02-03 14:20:52 +0200

    refactor(terms-popup.component) refactor indents

--- src/app/auth/components/popups/terms-popup/terms-popup.component.ts
+++ src/app/auth/components/popups/terms-popup/terms-popup.component.ts
@@ -8,8 +8,6 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class TermsPopupComponent implements OnInit {
 
-
-
   constructor(@Inject(MAT_DIALOG_DATA) public data) { }
 
   ngOnInit(): void {
