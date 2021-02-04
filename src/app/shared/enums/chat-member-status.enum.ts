88ee12bd510513a5a272578aadf7d028780f58ba
Almost Infinity <its.alfinity@gmail.com>
2021-02-04 13:27:25 +0200

    feat(chat-member-model): add status field

---
+++ src/app/shared/enums/chat-member-status.enum.ts
@@ -0,0 +1,8 @@
+export enum EChatMemberStatus {
+  Creator = 'creator',
+  Admin = 'admin',
+  Member = 'member',
+  Restricted = 'restricted',
+  Left = 'left',
+  Kicked = 'kicked',
+}
--- src/app/shared/models/conversation-member.model.ts
+++ src/app/shared/models/conversation-member.model.ts
@@ -1,7 +1,9 @@
 import { UserInfo } from './user-info.model';
+import { EChatMemberStatus } from '../enums/chat-member-status.enum';
 
 export interface ConversationMemberModel {
   user: UserInfo;
+  status: EChatMemberStatus;
   isNotification: boolean;
   isMarked: boolean;
 }
